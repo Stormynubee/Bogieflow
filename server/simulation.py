c13f51f534ef566ed066118dcbd304de4d1fdaa9
Hansraj Tiwari <stormylalala2233@gmail.com>
2026-06-09 10:00:00 +0530

    feat: implement simulation loop skeleton

---
+++ server/simulation.py
@@ -0,0 +1,7 @@
+# server/simulation.py
+class SimulationEngine:
+    def __init__(self, on_event=None):
+        self.on_event = on_event
+        self.segments = {}
+    def tick(self):
+        pass
